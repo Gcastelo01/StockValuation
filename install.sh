d4af4a29a892ae41ffdf4a242ef878545f637518
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-04-26 19:39:57 -0300

    fix: :bug: Bug shell script
    Correção de bug no script que fazia com que condições não fossem reconhecidas.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "[+] Criando ambiente virtual Python3 para execução do script..."
 
 python3 -m venv venv
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "[!] Erro criando VENV. Verifique sua instalação de python e tente novamente"
     exit 1
@@ -13,15 +13,22 @@ fi
 
 source ./venv/bin/activate
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
-    echo "[!] Erro ativando VENV. Abortando instalação."
-    exit 1
+    echo "[!] Erro ativando VENV. Deseja tentar instalar o gerenciador de venv python? [y/n]"
+    read $resp
+    if [ $resp -ne 'y' ]
+    then
+        apt install python3.10-venv -y
+        python3 -m venv venv
+    else
+        exit 1
+    fi
 fi
 
 pip install -r requirements.txt
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "[!] Erro instalando dependências. Tente instalar manualmente via $ pip install -r requirements.txt"
     exit 1
@@ -30,7 +37,7 @@ fi
 # Instalando dependências do Node.js
 npm install
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "[!] Erro instalando dependências NPM. Tente instalar manualmente via $npm install"
     exit 1
