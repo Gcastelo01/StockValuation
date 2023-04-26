96db1fb8f0179c6567d965cd4309aa7dc862b1b7
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-04-26 20:25:30 -0300

    Feat :sparkles: INstalação do quarto inserida no script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Criando ambient virtual e instalando dependências do pip
 echo "[+] Criando ambiente virtual Python3 para execução do script..."
@@ -41,7 +41,54 @@ npm install
 
 if [ $? -ne 0 ]
 then
-    echo "[!] Erro instalando dependências NPM. Tente instalar manualmente via $npm install"
+    echo "[!] Erro instalando dependências NPM. Talvez o node não esteja instalado."
+    read -p "[?] Deseja instalar o NodeJS e o NPM?. [y/n]" resp
+
+    if [ resp = 'y' ]
+    then
+        apt install nodejs
+        
+        if [ $? -ne 0 ]
+        then
+            echo "[!] Erro ao instalar NodeJS!"
+            exit 1
+        fi
+
+        echo "[+] NodeJS instalado com sucesso!"
+
+        apt install npm
+
+        if [ $? -ne 0 ]
+        then
+            echo "[!] Erro ao instalar NPM!"
+            exit 1
+        fi
+        echo "[+] NodeJS instalado com sucesso!"
+        npm install
+    else
+        exit 1
+    fi
+fi
+
+# Instalando QUARTO (framework necessário para compilar notebooks)
+echo "[+] Instalando framework QUARTO"
+curl -LO https://quarto.org/download/latest/quarto-linux-amd64.deb
+dpkg -i quarto-linux-amd64.deb
+
+if [ $? -ne 0]
+then
+    echo "[!] Erro na instalação do quarto! Acesse https://quarto.org/docs/get-started/ e instale manualmente."
+    exit 1
+fi
+
+echo "[+] Framework QUARTO instalado com sucesso!"
+
+echo "[+] Instalando TinyTex"
+quarto install tinytex
+
+if [ $? -ne 0]
+then
+    echo "[!] Erro na instalação do TinyTex!"
     exit 1
 fi
 
