8c86c1db779c12304b88408a6092d4a0bcbd8205
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-05-09 15:49:28 -0300

    feat: :sparkles: Geração de análises
    Script responsável por gerar análises

--- modules/setter.py
+++ modules/setter.py
@@ -1,8 +1,13 @@
 from os import system
-
+from os.path import abspath
 
 class Setter():
-    def __init__(self, ticker, mailer) -> None:
+    def __init__(self, ticker=None, safety=0.3) -> None:
         self.__ticker = ticker
-        self.__mailer = mailer
+        self.__safety = safety
 
+    def generate_analysis(self) -> None:
+        DATAPATH = abspath('.')
+
+        system(f"echo 'TICKER={self.__ticker}\nSAFETY={self.__safety}' > .env")
+        system(f"quarto render {DATAPATH}/assets/Evaluator.ipynb --to pdf --execute --output {self.__ticker}-analysis.pdf")
