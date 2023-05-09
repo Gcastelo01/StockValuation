8b05e73ad9216ed9370d21c78db6d8d6f7ad8200
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-05-09 16:25:02 -0300

    feat: :sparkles: Script de configuração
    Script para configurar serviço de monitoramento no sistema do usuário

---
+++ scripts/config.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Testando existência de diretório temporário
+if [ ! -d "$PWD/temp" ]; then
+    mkdir $PWD/temp
+fi
+
+# Criando serviço de monitoramento
+VENV_DIR="$PWD/venv/bin/python3"
+SCR_DIR="$PWD/monitor.py"
+SERVICE_DESC="[Unit]\nDescription=Monitoramento de E-mail\nAfter=network.target\n\n[Service]\nExecStart=$VENV_DIR $SRC_DIR\nWorkingDirectory=$SRC_DIR\nRestart=always\n\n[Install]\nWantedBy=multi-user.target"
+
+touch ./assets/mailmonitor.service
+echo -e $SERVICE_DESC > ./assets/mailmonitor.service
+
+cp ./assets/mailmonitor.service /etc/systemd/system/
+
+# Iniciando serviço
+systemctl daemon-reload
+systemctl start mailmonitor
+systemctl status mailmonitor
+
