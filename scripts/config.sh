c9de413c8aa8e7ddca7b598379db65903b8bed61
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-05-10 13:52:19 -0300

    fix: :bug: Correção de bugs na criação do serviço

--- scripts/config.sh
+++ scripts/config.sh
@@ -9,14 +9,14 @@ fi
 VENV_DIR="$PWD/venv/bin/python3"
 SRC_DIR="$PWD/monitor.py"
 
-SERVICE_DESC="[Unit]\nDescription=Monitoramento de E-mail\nAfter=network.target\n\n[Service]\nExecStart=$VENV_DIR $SRC_DIR \nWorkingDirectory=$SRC_DIR \nRestart=always\n\n[Install]\nWantedBy=multi-user.target"
+SERVICE_DESC="[Unit]\nDescription=Monitoramento de E-mail\nAfter=network.target\n\n[Service]\nExecStart=$VENV_DIR $SRC_DIR \nWorkingDirectory=$PWD \nRestart=always\n\n[Install]\nWantedBy=multi-user.target"
 
 touch ./assets/mailmonitor.service
 echo -e $SERVICE_DESC > ./assets/mailmonitor.service
 
-cp ./assets/mailmonitor.service /etc/systemd/system/
+# cp ./assets/mailmonitor.service /etc/systemd/system/
 
-# Iniciando serviço
-systemctl daemon-reload
-systemctl start mailmonitor
-systemctl status mailmonitor
+# # Iniciando serviço
+# systemctl daemon-reload
+# systemctl start mailmonitor
+# systemctl status mailmonitor
--- scripts/netconfig.sh
+++ scripts/netconfig.sh
@@ -2,9 +2,9 @@
 
 read -p "E-mail para o servidor: " emailServidor
 
-read -p "Digite a senha numérica para o acesso automático" senha
+read -p "Digite a senha numérica para o acesso automático: " senha
 
 touch .netconfig
 
-echo -e "IMAP_HOST='imap.google.com'\nSMTP_PORT=587\nIMAP_USER='$emailServidor'\nIMAP_PASSWD='$senha'
+echo -e "IMAP_HOST='imap.gmail.com'\nSMTP_PORT=587\nIMAP_USER='$emailServidor'\nIMAP_PASSWD='$senha'
 SMTP_SERVER='smtp.gmail.com'" > .netconfig
