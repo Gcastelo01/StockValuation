d8e430f9a9f935d2c8078a81c4ec4161cc7f1540
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-05-10 10:21:34 -0300

    test: :white_check_mark: Adicionando testes ao main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import argparse
 import modules.mailer as mail
 import modules.setter as set
 
+from os import system
+
 parser = argparse.ArgumentParser(description="Programa entrypoint para o script de envio")
 
-parser.add_argument("-t", "--ticker", required=True)
+parser.add_argument("-t", "--ticker")
 parser.add_argument('-a', "--alert", action='store_true')
+parser.add_argument('-m', "--monitor", action='store_true')
 
 args = parser.parse_args()
 
-ss = set.Setter(args.ticker, 0.3)
-
-ss.generate_analysis()
-
-# m = mail.Mailer(args.ticker,
+if args.monitor:
+    system("python3 monitor.py")
