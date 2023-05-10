f5d08c41789d199d03ddf9afcfd4739a1c3ca1fa
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-05-10 14:21:05 -0300

    test: :test_tube: Novo teste

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ parser = argparse.ArgumentParser(description="Programa entrypoint para o script 
 parser.add_argument("-t", "--ticker")
 parser.add_argument('-a', "--alert", action='store_true')
 parser.add_argument('-m', "--monitor", action='store_true')
+parser.add_argument('-s', '--test_setter', action='store_true')
 
 args = parser.parse_args()
 
 if args.monitor:
     system("python3 monitor.py")
+
+if args.test_setter:
+    s = set.Setter(args.ticker)
+    s.generate_analysis()
