ae626fde6dde4be92520a6da12dd5eba7482d6b6
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-05-10 14:20:54 -0300

    fix: :bug: Correção de bug
    Correção de bug que causava um crash do serviço quando o arquivo .env não existia

--- modules/setter.py
+++ modules/setter.py
@@ -1,5 +1,5 @@
 from os import system
-from os.path import abspath
+from os.path import abspath, exists
 
 class Setter():
     def __init__(self, ticker=None, safety=0.3) -> None:
@@ -8,6 +8,10 @@ class Setter():
 
     def generate_analysis(self) -> None:
         DATAPATH = abspath('.')
+        ENVPATH = DATAPATH + '/.env'
 
-        system(f"echo 'TICKER={self.__ticker}\nSAFETY={self.__safety}' > .env")
+        if not exists(ENVPATH):
+            system("touch .env")
+        
+        system(f"echo 'TICKER={self.__ticker}\nSAFETY={self.__safety}' > {DATAPATH}/.env")
         system(f"quarto render {DATAPATH}/assets/Evaluator.ipynb --to pdf --execute --output {self.__ticker}-analysis.pdf")
