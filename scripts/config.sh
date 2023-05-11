0e46cdeb6b8dac98e4db905b4db7c1229eb09d00
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-05-11 09:00:11 -0300

    fix: :bug: Correção de caminho dos logs

--- scripts/config.sh
+++ scripts/config.sh
@@ -11,9 +11,10 @@ SRC_DIR="$PWD/monitor.py"
 
 data=$(date +"%d/%m/%Y")
 
-SERVICE_DESC="[Unit]\nDescription=Monitoramento de E-mail\nAfter=network.target\n\n[Service]\nExecStart=$VENV_DIR $SRC_DIR \nWorkingDirectory=$PWD\nRestart=always\nStandardOutput=append:/var/log/mailmonitor-service-$data.log\nStandardError=append:/var/log/mailmonitor-service-error-$data.log\n[Install]\nWantedBy=multi-user.target"
+SERVICE_DESC="[Unit]\nDescription=Monitoramento de E-mail\nAfter=network.target\n\n[Service]\nExecStart=$VENV_DIR $SRC_DIR \nWorkingDirectory=$PWD\nRestart=always\nStandardOutput=append:$PWD/logs/mailmonitor-service-$data.log\nStandardError=append:$PWD/logs/mailmonitor-service-error-$data.log\n[Install]\nWantedBy=multi-user.target"
 
 touch ./assets/mailmonitor.service
+
 echo -e $SERVICE_DESC > ./assets/mailmonitor.service
 
 cp ./assets/mailmonitor.service /etc/systemd/system/
